5451c9a97c57966ea16d9f6779f0d66877067bd9
AndryBbz <andrybarbaza@gmail.com>
2018-10-24 16:25:38 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,16 @@
 var result
-                fetch('https://nominatim.openstreetmap.org/search?q=paris&format=json').then(
+<<<<<<< HEAD
+fetch('https://nominatim.openstreetmap.org/search?q=paris&format=json').then(
+        function(response){
+            response.json().then(function(data){result = data})
+        }
+    )
+=======
+    fetch('https://fr.openfoodfacts.org/api/v0/produit/3029330003533.json').then(
                     function(response){
-                    response.json().then(function(data){result = data})
+                        response.json().then(function(data){result = data})
+                        
+                
                     }
                 )
+>>>>>>> 5268b0dbfe9f9450e8cb925e9bb4b0944aa915d2
