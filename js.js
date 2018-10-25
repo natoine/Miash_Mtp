b4ca93ef8891682a96af8b85c29e1abfb2c81d49
BenMlahA <35455977+BenMlahA@users.noreply.github.com>
2018-10-25 14:04:54 +0200

    connexion a l'api recipe

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
-var result
-    fetch('https://fr.openfoodfacts.org/api/v0/produit/3029330003533.json').then(
+var result ; 
+var app_id = "6b803eba" ; 
+var app_key = "79b5efc1599e7d80343c1669e67bcd30" ;
+var url ='https://api.edamam.com/search?q=chicken&app_id='+app_id+'&app_key='+app_key;
+    fetch(url).then(
                     function(response){
                         response.json().then(function(data){result = data})
                         
