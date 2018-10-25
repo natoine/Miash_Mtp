fb3029a6bc1323316b987931bdd2a2286393682c
AndryBbz <andrybarbaza@gmail.com>
2018-10-25 10:30:05 +0200

    Update js.js

--- js.js
+++ js.js
@@ -7,3 +7,12 @@ fetch('https://fr.openfoodfacts.org/api/v0/produit/3029330003533.json').then(
                     
                     }
                 )
+var retour
+fetch('https://github.com/openfoodfacts/cordova-app/blob/master/www/off.js').then(
+                    function(response){
+                        response.json().then(function(data){retour = data})
+                        
+                
+                    
+                    }
+                )
