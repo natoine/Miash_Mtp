b691320b38e2e67a317138b83cbaf90b7a38fd8d
AndryBbz <andrybarbaza@gmail.com>
2018-12-20 20:51:51 +0100

    fill tab
    Co-Authored-By: benmlaha <benmlaha@users.noreply.github.com>

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
 let countries; //will contain "fetched" data
 const countriesList = document.getElementById("countries");
 var identifier;
+var capital;
 
 fetch("https://restcountries.eu/rest/v2/all")
 	.then(res=> res.json())
 	.then(data => initialize(data))
+	//.then(
 	.catch(err => console.log("Error:",err));
 
 function initialize(countriesData){
@@ -15,16 +17,58 @@ function initialize(countriesData){
     $(document).ready(function(){
         $('#countries').formSelect();
     });
+
 }
 
 var id_select = document.getElementById("countries");
 var id_select = document.getElementById("informations");
 
-/*document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('select[name="ice-cream"]').onchange=pull_info;
-},false);*/
 
 function pull_info(id){
-	alert(id);
-	
+	capital = id;
+	for ( var i = 0 ; i < countries.length ; i++){
+		if (countries[i].capital == capital){
+			var vraiNom = countries[i].nativeName;
+			var continent = countries[i].region;
+			var monnaie = '';
+			countries[i].currencies.forEach(function(i){
+				monnaie += i.code + ' ' +i.symbol+"\n";
+			});
+			var langue = '';
+			countries[i].languages.forEach(function(i){
+				langue += i.name+"\n";
+			});
+			
+			var tr_capital = document.getElementById('capital');
+			var td_capital = document.createElement('td');
+			var td_content_capital = document.createTextNode(capital);
+			td_capital.appendChild(td_content_capital);
+			tr_capital.appendChild(td_capital);
+			
+			var tr_VN = document.getElementById('nativeName');
+			var td_VN = document.createElement('td');
+			var td_content_VN = document.createTextNode(vraiNom);
+			td_VN.appendChild(td_content_VN);
+			tr_VN.appendChild(td_VN);
+			
+			var tr_currencie = document.getElementById('currencies');
+			var td_currencie = document.createElement('td');
+			var td_content_currencie = document.createTextNode(monnaie);
+			td_currencie.appendChild(td_content_currencie);
+			tr_currencie.appendChild(td_currencie);
+			
+			var tr_continent = document.getElementById('region');
+			var td_continent = document.createElement('td');
+			var td_content_continent = document.createTextNode(continent);
+			td_continent.appendChild(td_content_continent);
+			tr_continent.appendChild(td_continent);
+			
+			var tr_langue = document.getElementById('languages');
+			var td_langue = document.createElement('td');
+			var td_content_langue = document.createTextNode(langue);
+			td_langue.appendChild(td_content_langue);
+			tr_langue.appendChild(td_langue);
+			
+		}
+	}
 }
