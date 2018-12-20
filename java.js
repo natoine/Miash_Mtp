b7a4e196274362828a8714d6e24f461e8a9f8449
AndryBbz <andrybarbaza@gmail.com>
2018-12-20 11:00:53 +0100

    select good

--- java.js
+++ java.js
@@ -1,29 +1,29 @@
 //global variables
 let countries; //will contain "fetched" data
-
+const countriesList = document.getElementById("countries");
 /*fetch("https://restcountries.eu/rest/v2/all")
-.then(function(res){
-	//console.log(res.json());
-	return res.json();
-})
-.then(function(data){
-	//console.log(data);
-	initialize(data);
-})
-.catch(function(err){
-	console.log("Error:",err);
-});
-*/ 
+	.then(function(res){
+		//console.log(res.json());
+		return res.json();
+	})
+	.then(function(data){
+		//console.log(data);
+		initialize(data);
+	})
+	.catch(function(err){
+		console.log("Error:",err);
+	});
+	*/ 
 fetch("https://restcountries.eu/rest/v2/all")
-.then(res=> res.json())
-.then(data => initialize(data))
-.catch(err => console.log("Error:",err));
+	.then(res=> res.json())
+	.then(data => initialize(data))
+	.catch(err => console.log("Error:",err));
 
 function initialize(countriesData){
 	countries = countriesData;
 	let options = "";
-	for (let i = 0; i<countries.length ; i++){
-		options += `<option value="` + countries[i].alpha3Code + `">` +countries[i].name + `</option>`;
-	}
-	document.getElementById("countries").innerHTML = options;
+	countries.forEach(country => $('#countries').append(new Option(country.name, country.alpha3Code)));
+	//$(document).ready(function(){
+		//$('#countries').formSelect();
+	//});
 }
