b3da0619e0a7770548a068b5d2260de25909abce
AndryBbz <andrybarbaza@gmail.com>
2018-12-20 19:16:06 +0100

    pull_info

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 let countries; //will contain "fetched" data
 const countriesList = document.getElementById("countries");
+var identifier;
 
 fetch("https://restcountries.eu/rest/v2/all")
 	.then(res=> res.json())
@@ -15,3 +16,14 @@ function initialize(countriesData){
         $('#countries').formSelect();
     });
 }
+
+var id_select = document.getElementById("countries");
+var id_select = document.getElementById("informations");
+
+/*document.addEventListener('DOMContentLoaded',function() {
+    document.querySelector('select[name="ice-cream"]').onchange=pull_info;
+},false);
+
+function pull_info(id){
+	alert(id.value);
+}*/
